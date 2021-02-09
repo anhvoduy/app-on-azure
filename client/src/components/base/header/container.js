d9cdc37a00d33a1f3cb53c7c68cd69d216b1835b
anhvoduy <voduyanh1984@gmail.com>
2021-02-09 21:04:32 +0700

    - update css background dark color: #6c757d

--- client/src/components/base/header/container.js
+++ client/src/components/base/header/container.js
@@ -4,7 +4,7 @@ const Container = function() {
   return (
     <nav className='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
       <div className='container'>
-        <a className='navbar-brand' href='/'>UFM QTKD K17</a>
+        <a className='navbar-brand' href='/'>VNI Online 365</a>
         <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarResponsive' aria-controls='navbarResponsive' aria-expanded='false' aria-label='Toggle navigation'>
           <span className='navbar-toggler-icon'></span>
         </button>
